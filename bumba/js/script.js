40e59b7fd5987b17913d0655ddadba412ab56315
chebon254 <kelvinchebon90@gmail.com>
2023-02-23 17:16:15 +0300

    Repair

---
+++ bumba/js/script.js
@@ -0,0 +1,9 @@
+/* === Overlay === */
+function openContact() {
+    document.getElementById("MenuOverlay").style.display = "block";
+}
+
+function closeContact() {
+    document.getElementById("MenuOverlay").style.display = "none";
+}
+/* === Overlay === */
