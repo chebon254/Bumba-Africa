b3b59d5c3ae4e975ada66818c44d2646ec27a7d4
chebon254 <kelvinchebon90@gmail.com>
2023-02-23 22:12:14 +0300

    Code Changes

--- bumba/js/script.js
+++ bumba/js/script.js
@@ -7,3 +7,33 @@ function closeContact() {
     document.getElementById("MenuOverlay").style.display = "none";
 }
 /* === Overlay === */
+
+/* == Blogs == */
+var slideIndex = 1;
+        showSlides(slideIndex);
+
+        function plusSlides(n) {
+        showSlides(slideIndex += n);
+        }
+
+        function currentSlide(n) {
+        showSlides(slideIndex = n);
+        }
+
+        function showSlides(n) {
+        var i;
+        var slides = document.getElementsByClassName("blogs");
+        var dots = document.getElementsByClassName("number");
+        if (n > slides.length) {slideIndex = 1}    
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";  
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+        slides[slideIndex-1].style.display = "block";  
+        dots[slideIndex-1].className += " active";
+
+        }
+/* == || Blogs == */
